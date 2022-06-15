7dd308b3cb34bd6f6bcbffac6be70d0c6359bd61
issaice <hawo1551issa@gmail.com>
2022-06-14 22:24:33 -0700

    Finished core features!

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import * as React from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
 import { createDataSet } from "./data/dataset"
 import "./App.css"
+import Header from "./components/Header/Header"
+import Instructions from "./components/Instructions/Instructions"
+import Chip from "./components/Chip/Chip"
+import { useState } from "react";
+import NutritionalLabel from "./components/NutritionalLabel/NutritionalLabel"
+
 
 // don't move this!
 export const appInfo = {
@@ -21,6 +27,21 @@ export const appInfo = {
 const { data, categories, restaurants } = createDataSet()
 
 export function App() {
+  const [selectedCategory, chosenCategory] = useState(null);
+  const categoryHandler = (category) => {
+    chosenCategory(category);
+  };
+  const [selectedRestaurant, chosenRestaurant] = useState(null);
+  const restaurantHandler = (restaurant) => {
+    chosenRestaurant(restaurant);
+  };
+  const currentMenuItems = data.filter((item) => {
+    return item.food_category === selectedCategory && item.restaurant === selectedRestaurant;
+  })
+  const [selectedMenuItem, chosenMenuItem] = useState(null);
+  const menuItemHandler = (item) => {  
+    chosenMenuItem(item);
+  };
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
@@ -28,30 +49,46 @@ export function App() {
         <div className="categories options">
           <h2 className="title">Categories</h2>
           {/* YOUR CODE HERE */}
+          {categories.map((category) => {
+            return <Chip label={category} key={category} 
+            onClick = {() => categoryHandler(category)}
+            isActive={selectedCategory === category}/>
+          })}
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
         {/* HEADER GOES HERE */}
-
+        <Header title={appInfo.title}
+            tagline={appInfo.tagline} description={appInfo.description} />
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">{restaurants.map((restaurant) => {
+            return <Chip key={restaurant} label={restaurant} 
+            onClick = {() => restaurantHandler(restaurant)}
+            isActive = {selectedRestaurant === restaurant}/>
+          })}</div>
         </div>
 
         {/* INSTRUCTIONS GO HERE */}
-
+        <Instructions instructions={appInfo.instructions.start} />
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
             {/* YOUR CODE HERE */}
+            {currentMenuItems.map((item, idx) => {
+              return <Chip key={idx} label={item.item_name}
+              onClick = {() => menuItemHandler(item)}
+              isActive = {selectedMenuItem === item}/>
+            })}
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts">{selectedMenuItem ? <NutritionalLabel item={selectedMenuItem}/> : null }
+          </div>
         </div>
 
         <div className="data-sources">
@@ -60,6 +97,7 @@ export function App() {
       </div>
     </main>
   )
+  
 }
 
 export default App
--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,11 +1,14 @@
 import * as React from "react"
 import "./Chip.css"
 
-export function Chip({ label = "", isActive = false }) {
+export function Chip({ label = "", isActive = false, onClick}) {
+  let buttonClassName = isActive ? "active chip" : "chip"
+  //`chip ${isActive ? "active" : ""}`
   return (
-    <button className="chip">
-      <p className="label"></p>
+    <button onClick= {onClick} className={buttonClassName} >
+      <p className="label">{label}</p>
       <span className="close" role="button">{`X`}</span>
+      
     </button>
   )
 }
